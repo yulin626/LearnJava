71da7accbc6a6454508c51158663b9fa92d530db
lin <yulin@ixinwu.com>
2018-08-30 11:09:27 +0800

    Initial commit

---
+++ src/main/java/com/example/demo/AwareService.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.context.ResourceLoaderAware;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AwareService implements BeanNameAware,ResourceLoaderAware {
+
+    private String beanName;
+    private ResourceLoader loader;
+
+    @Override
+    public void setBeanName(String name) {
+        this.beanName = name;
+    }
+
+    @Override
+    public void setResourceLoader(ResourceLoader resourceLoader) {
+        this.loader = resourceLoader;
+    }
+}
---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+//        SpringApplication.run(DemoApplication.class, args);
+
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(TaskExecutorConfig.class);
+
+        AsyncTaskService asyncTaskService = context.getBean(AsyncTaskService.class);
+
+        for (int i=0;i<10;i++){
+            asyncTaskService.executeAsyncTask(i);
+            asyncTaskService.executeAsyncTaskPlus(i);
+        }
+        context.close();
+    }
+}
