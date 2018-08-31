4e6937e22b700f663855654e5d7109d01731a266
lin <yulin@ixinwu.com>
2018-08-31 16:40:16 +0800

    schedule

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.ComponentScan;
 public class DemoApplication {
 
     public static void main(String[] args) {
-//        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(DemoApplication.class, args);
 
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(TaskExecutorConfig.class);
 
